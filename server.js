d8054c20797a87f2e4558eb0633e258dd4b56597
AnaM93 <ana.18_03@yahoo.com>
2023-10-22 14:02:50 -0700

    added two endpoints

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
 
 
 const app = express();
@@ -23,6 +24,13 @@ const database = {
             entries: 0,
             joined: new Date()
         }
+    ],
+    login : [
+        {
+            id : '987',
+            hash: '',
+            email: 'john@gmail.com'
+        }
     ]
 }
 
@@ -31,6 +39,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+ bcrypt.compare("apples", '$2a$10$Pg9JHjQ1Z/RRzRR2bT7qWu5.wiHKxRMCSZKAaQDE.IEef0PK5C3g.', function(err, res) {
+    console.log ('first guess', res)
+});
+bcrypt.compare("veggies", '$2a$10$Pg9JHjQ1Z/RRzRR2bT7qWu5.wiHKxRMCSZKAaQDE.IEef0PK5C3g.', function(err, res) {
+    console.log ('second guess', res)
+});
     if(req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password){
             res.json('success')
@@ -42,6 +56,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const {email, name, password} = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+        console.log(hash)
+    });
     database.users.push({
             id: '125',
             name: name,
@@ -52,6 +69,38 @@ app.post('/register', (req, res) => {
     })
     res.json(database.users[database.users.length-1])
 })
+
+app.get('/profile/:id', (req, res) => {
+    const {id} = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        } 
+        
+    })
+    if (!found) {
+        res.status(400).json('not found')
+    }
+})
+
+app.post('/image', (req, res) => {
+    const {id} = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries ++
+            return res.json(user.entries);
+        } 
+    })
+    if (!found) {
+        res.status(400).json('not found')
+    }
+})
+
+
 
 app.listen(3000, ()=> {
     console.log('app running at port 3000')
